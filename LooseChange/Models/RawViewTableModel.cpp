952fadf364c5af7706f5018df84de9f4c326d75b
gojoe262 <gojoe262@hotmail.com>
2014-12-19 20:36:46 -0600

    RawViewTableModel added... but not finished.

---
+++ LooseChange/Models/RawViewTableModel.cpp
@@ -0,0 +1,5 @@
+#include "RawViewTableModel.h"
+
+RawViewTableModel::RawViewTableModel()
+{
+}
---
+++ LooseChange/Models/RawViewTableModel.h
@@ -0,0 +1,10 @@
+#ifndef RAWVIEWTABLEMODEL_H
+#define RAWVIEWTABLEMODEL_H
+
+class RawViewTableModel
+{
+public:
+    RawViewTableModel();
+};
+
+#endif // RAWVIEWTABLEMODEL_H
