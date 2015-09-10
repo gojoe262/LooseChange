c28a2ec36832d076d3a0aa0ab0b8864637da59ac
Joe <gojoe262@hotmail.com>
2015-09-09 22:37:48 -0500

    Add navigation for index and transaction-table
    https://trello.com/c/zwTsVmNo

--- js/transaction-table.js
+++ js/transaction-table.js
@@ -1,9 +1,54 @@
 var transactionTable = function(){
+    var jsonCacher;
     function init(){
-        $('#TransactionTable').footable();
+        setupTable();
+        jsonCacher = new gJsonCacher();
+        authorize(true);//Skip popup
+    }
+
+    function authorize(skipPopup){
+        jsonCacher.authorize({
+            immediate: skipPopup
+        }).done(function(){
+            $('#pageContent').show();
+        }).fail(function(){
+            $('#gapiDialog').dialog({
+                modal: true,
+                width: 'auto',
+                height: 'auto',
+                buttons: [
+                    {
+                        text: "Login with Google",
+                        icons: {primary: "ui-icon-triangle-1-e"},
+                        click: function(){
+                            authorize(false);
+                            $(this).dialog('close');
+                        }
+                    },
+                    {
+                        text: "Cancel",
+                        click: function(){
+                            $(this).dialog('close');
+                        }
+                    }
+                ]
+            }).dialog('open');
+        });
+    }
+
+    function onResize() {
+        $("#gapiDialog").dialog("option", "position", {my: "center", at: "center", of: window});
+    }
+
+    function setupTable(){
+        $('#transactionTable').footable();
+        setTimeout(function () {
+            $('#transactionTable').trigger('footable_resize');
+        },10);
     }
 
     return {
-        init: init
+        init: init,
+        onResize: onResize
     };
 };
